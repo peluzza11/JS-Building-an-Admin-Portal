3a25edbd61ef9c62d8c60359a78c654ad9ac6cfd
Peluzza11 <ro89585@gmail.com>
2023-11-13 11:39:49 -0800

    hola

--- public/index.js
+++ public/index.js
@@ -1,7 +1,17 @@
 async function main() {
 
-    let response = await fetch('http://localhost:3001/listBooks')
-    let books = await response.json()
+    let response = await fetch('http://localhost:3001/updateBook', {
+        method: "PATCH",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "id": 3,
+            "title": "The Annals of Arathrae",
+        }),
+    });
+    let updatedBook = await response.json();
+    console.log(updatedBook)
 
     books.forEach(renderBook)
 }
